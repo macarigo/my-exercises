b31b6b7edc7538a389160f429836f41c2fe1efde
Mario Goncalves <macarigo@gmail.com>
2024-10-11 18:48:17 +0100

    update to include Range test exercise

---
+++ linked-list/src/io/codeforall/bootcamp/Range.java
@@ -0,0 +1,17 @@
+package io.codeforall.bootcamp;
+
+import java.util.Iterator;
+
+public class Range implements Iterable<T> {
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+}
+
---
+++ linked-list/src/io/codeforall/bootcamp/RangeTest.java
@@ -0,0 +1,34 @@
+package io.codeforall.bootcamp;
+
+import java.util.Iterator;
+
+public class RangeTest implements Iterable<T> {
+    public static void main(String[] args) {
+
+        Range r = new Range(-5, 5);
+
+        System.out.println("--- USING ITERATOR ---");
+        Iterator<Integer> it = r.iterator();
+
+        /* while (it.hasNext()) {
+
+        int i = it.next();
+
+        if (i == 1 || i == 2 || i == 3) {
+        it.remove();
+        }
+    }*/
+        for (Integer i : r) {
+            System.out.println("Iterated: " + i);
+
+
+        }
+        @Override
+        public Iterator<T> iterator<>() {
+            return null;
+
+        }
+
+
+    }
+}
